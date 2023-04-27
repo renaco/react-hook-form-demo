506bc8798f60c3e4e11fb7efc3ee65679ef1a7f0
renaco <renanromero@gmail.com>
2023-04-27 00:36:03 -0500

    feat: submit data form

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+
+let renderCount = 0;
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
 
 const YouTubeForm = () => {
 
-  const form = useForm();
-  // const { register, handleSubmit, errors } = form;
-  // const { name, ref, onChange, onBlur } = register("username");
-  const { register } = form;
- 
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  const onSubmit = (data: FormValues) => {
+    console.log('onSubmit form', data)
+  }
+
+  renderCount++
+
   return (
     <div>
-      <form action="">
+      <h1>YouTube Form ({renderCount / 2})</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
-        
+
         <label htmlFor="email">Email</label>
         <input type="text" id="email" {...register("email")} />
-        
+
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   )
 }
