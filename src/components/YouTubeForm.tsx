d7e6c2e820d3f8284c4c13b0215209b1f7ff939b
renaco <renanromero@gmail.com>
2023-06-21 20:47:59 -0500

    feat: add selected component

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,72 +1,117 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { DevTool } from '@hookform/devtools';
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 let renderCount = 0;
+
+let countries = [
+  { label: "Bangladesh", value: "Bangladesh" },
+  { label: "India", value: "India" },
+  { label: "China", value: "China" },
+  { label: "Finland", value: "Finland" },
+];
 
 type FormValues = {
   username: string;
   email: string;
   channel: string;
+  country: string;
 };
 
 const YouTubeForm = () => {
-
   const form = useForm<FormValues>();
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
 
   const onSubmit = (data: FormValues) => {
-    console.log('onSubmit form', data)
-  }
+    console.log("onSubmit form", data);
+  };
 
-  renderCount++
+  useEffect(() => {
+    handleSubmit(onSubmit);
+  }, []);
+
+  renderCount++;
 
   return (
     <div>
       <h1>YouTube Form ({renderCount / 2})</h1>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <div className="form-control">
-
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" {...register("username", {
-            required: {
-              value: true,
-              message: 'Username is required',
-            },
-          })} />
-          <p className='error'>{errors.username?.message}</p>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "Username is required",
+              },
+            })}
+          />
+          <p className="error">{errors.username?.message}</p>
         </div>
 
         <div className="form-control">
-
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" {...register("email", {
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: 'Invalid email address',
-            }
-          })} />
-          <p className='error'>{errors.email?.message}</p>
+          <input
+            type="text"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address",
+              },
+              validate: {
+                notAdmin: (fieldValue) => {
+                  return fieldValue !== "admin@example.com" || "Nice try!";
+                },
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
         </div>
 
         <div className="form-control">
-
           <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" {...register("channel", {
-            required: {
-              value: true,
-              message: 'Channel is required',
-            },
-          })} />
-          <p className='error'>{errors.channel?.message}</p>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              required: {
+                value: true,
+                message: "Channel is required",
+              },
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
 
+        <div className="form-control">
+          <label htmlFor="country">Country</label>
+          <select
+            id="country"
+            {...register("country", {
+              required: {
+                value: true,
+                message: "Country is required",
+              },
+            })}
+          >
+            <option value="">Select a country</option>
+            {countries.map((country) => (
+              <option key={country.value} value={country.value}>
+                {country.label}
+              </option>
+            ))}
+          </select>
+          <p className="error">{errors.channel?.message}</p>
         </div>
         <button>Submit</button>
       </form>
       <DevTool control={control} />
     </div>
-  )
-}
+  );
+};
 
-export default YouTubeForm
+export default YouTubeForm;
